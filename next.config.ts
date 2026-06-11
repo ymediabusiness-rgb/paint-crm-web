b7b645f4844bdce54701148bbc40fdf78e485138
ymediabusiness-rgb <ymedia.business@gmail.com>
2026-06-11 13:50:26 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-ignore: Next.js 16 turbopack config
-  turbopack: {
-    root: "C:\\Users\\User\\Documents\\CRM\\paint-crm-web",
-  },
+  // @ts-ignore
+  ...(process.env.NETLIFY ? {} : {
+    turbopack: {
+      root: "C:\\Users\\User\\Documents\\CRM\\paint-crm-web",
+    },
+  })
 };
 
 export default nextConfig;
